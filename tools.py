818173d2171a61ea37d7476940e9ba332e02e29c
worm2fed <worm2fed@mail.ru>
2017-04-29 21:30:38 +0300

    Added some new methods

--- tools.py
+++ tools.py
@@ -1,9 +1,39 @@
-import time
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-26
+# @Author  : Andrey Demidenko (worm2fed@mail.ru)
+# @Version : 1.0
 
-# Class to elapse time
+""" This is set of different tools
+"""
+
+__version__ = '1.0'
+__author__ = 'Andrey Demidenko'
+__docformat__ = 'reStructuredText'
+
+
+import time
+import sys
+import os
+
+
 class LeadTime(object):
-    def __enter__(self):
-        self._startTime = time.time()
-         
-    def __exit__(self, type, value, traceback):
-        print('Elapsed time: {:.3f} sec '.format(time.time() - self._startTime))
+	""" Class to elapse time
+	"""
+	def __enter__(self):
+		self._startTime = time.time()
+        
+
+	def __exit__(self, type, value, traceback):
+		print('Elapsed time: {:.3f} sec '.format(time.time() - self._startTime))
+
+
+# Method to clean up a console
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+# Method to quit
+quit = lambda mes='': sys.exit(mes)
+# Method to go back and stop function
+back = lambda: False
+stop = lambda: True
+# Get file extension
+extension = lambda f: os.path.splitext(f)
